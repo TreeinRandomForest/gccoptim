350f568a8717c61056a2c7023c3a8d4ab7ea112e
sanjay <treeinrandomforest@gmail.com>
2020-05-28 17:07:10 -0400

    fix test suite shell script

--- container-testsuite/run_experiments.sh
+++ container-testsuite/run_experiments.sh
@@ -3,24 +3,41 @@
 set -u
 set -e
 
+TEST_LOC=/home/user
+STORE_LOC=/home/user
+
 #setup batch mode
-cp /store/user-config.xml /etc/phoronix-test-suite.xml
+cp $STORE_LOC/store/user-config.xml /etc/phoronix-test-suite.xml
 
-for file in /store/PARAMS*
+while true
 do
-	PARAMFILE=$file
+	while [ ! -f "$STORE_LOC/store/PARAMSWRITTEN.log" ] #check if params done 
+	do
+		sleep 1
+		echo "Waiting for parameter generation"
+	done
 
-	export CFLAGS="-frecord-gcc-switches @${PARAMFILE}" #specific to redis
-	echo $CFLAGS
+	#???# rm /store/PARAMSWRITTEN.log
+	for file in $(find $STORE_LOC/store -regextype grep -regex "$STORE_LOC/store/PARAMS[0-9]*" -exec ls {} \;) #/store/PARAMS[0-9]*
+	do
+		PARAMFILE=$file
 
-	cd /phoronix-test-suite && ./phoronix-test-suite batch-benchmark redis #specific to redis
+		export CFLAGS="-frecord-gcc-switches @${PARAMFILE}" #specific to redis
+		echo $PARAMFILE
+		echo $CFLAGS
 
-	#Save results
-	FOLDERNAME=$(echo $PARAMFILE | awk -F/ '{print $NF}')
-	HOSTNAME=$(cat /proc/sys/kernel/hostname)
-	
-	mkdir -p /store/$HOSTNAME/$FOLDERNAME
-	cp $file /store/$HOSTNAME/$FOLDERNAME/
-	mv /var/lib/phoronix-test-suite/test-results /store/$HOSTNAME/$FOLDERNAME
-	mv /var/lib/phoronix-test-suite/installed-tests /store/$HOSTNAME/$FOLDERNAME
+		cd $TEST_LOC/phoronix-test-suite && ./phoronix-test-suite batch-benchmark redis #specific to redis
+
+		#Save results
+		FOLDERNAME=$(echo $PARAMFILE | awk -F/ '{print $NF}')
+		HOSTNAME=$(cat /proc/sys/kernel/hostname)
+		
+		mkdir -p $STORE_LOC/store/$HOSTNAME/$FOLDERNAME #output folder
+		mv $file $STORE_LOC/store/$HOSTNAME/$FOLDERNAME/ #param file
+		mv $TEST_LOC/.phoronix-test-suite/test-results $STORE_LOC/store/$HOSTNAME/$FOLDERNAME #test results
+		mv $TEST_LOC/.phoronix-test-suite/installed-tests $STORE_LOC/store/$HOSTNAME/$FOLDERNAME #test binaries
+	done
+
+	rm $STORE_LOC/store/PARAMSWRITTEN.log
+	touch $STORE_LOC/store/TESTDONE.log
 done
