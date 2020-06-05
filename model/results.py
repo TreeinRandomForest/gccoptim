40f65665467710d23a47f6f2fda1303900780146
sanjay <treeinrandomforest@gmail.com>
2020-06-05 13:08:47 -0400

    functions for parsing xml results

---
+++ model/results.py
@@ -0,0 +1,39 @@
+import numpy as np
+import xml.etree.ElementTree as ET
+
+def read_results(loc):
+	filename = os.path.join(loc, 'composite.xml')
+
+	if os.path.exists(filename):
+		raise ValueException(f'File composite.xml not found in: {loc}')
+
+	r = parse_xml(filename)
+
+	return r
+
+def parse_xml(xml_file):
+	tree = ET.parse(xml_file)
+
+	root = tree.getroot()
+
+	c_results = [c for c in root if c.tag=='Result'] #c for children
+
+	results = []
+	for c in c_results: #loop over results tags
+		r = {}
+		for elem in c: #loop over results' children
+			if elem.tag == 'Data':
+				data = {}
+				for e in elem:
+					for entry in e:
+						if entry.tag=='Value':
+							data['value'] = float(entry.text)
+						if entry.tag=='RawString':
+							data['raw_values'] = [float(i) for i in entry.text.split(":")]
+			else:
+				data = elem.text
+
+			r[elem.tag] = data
+		results.append(r)
+
+	return results
