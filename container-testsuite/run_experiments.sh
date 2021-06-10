ead4858fae0a7d9da4d25b39f3abc5f380cfdd2c
sanjay <treeinrandomforest@gmail.com>
2021-06-10 17:41:49 -0400

    fixing some path stuff in dockerfile and script

--- container-testsuite/run_experiments.sh
+++ container-testsuite/run_experiments.sh
@@ -5,6 +5,7 @@ set -e
 
 TEST_LOC=/home/user #phoronix-test-suite
 STORE_LOC=/home/user/store #store (shared volume) specific to this container
+RESULT_LOC=/var/lib
 
 cp $STORE_LOC/user-config.xml /etc/phoronix-test-suite.xml
 $TEST_LOC/phoronix-test-suite/phoronix-test-suite enterprise-setup
@@ -15,7 +16,7 @@ echo $PARAMFILE
 echo $CFLAGS
 
 #look at bash scripts for test to ensure gcc is being used
-cd $TEST_LOC/phoronix-test-suite && ./phoronix-test-suite batch-benchmark redis #specific to redis
+cd $TEST_LOC/phoronix-test-suite && ./phoronix-test-suite batch-benchmark redis >& /home/user/store/main_log #specific to redis
 
-mv $TEST_LOC/.phoronix-test-suite/test-results $STORE_LOC #test results
-mv $TEST_LOC/.phoronix-test-suite/installed-tests $STORE_LOC #test binaries
+mv $RESULT_LOC/phoronix-test-suite/test-results $STORE_LOC #test results
+mv $RESULT_LOC/phoronix-test-suite/installed-tests $STORE_LOC #test binaries
